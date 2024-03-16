668091f99fe39d3f3e83d31bbeb225137b92ac93
dentropy <dentropy@protonmail.com>
2024-03-16 14:59:38 -0400

    I seem to have my grasp around RxDB now

--- test.js
+++ test.js
@@ -1,70 +1,92 @@
 
-import { createRxDatabase } from 'rxdb';
+import { createRxDatabase, addRxPlugin } from 'rxdb';
 import { getRxStorageDenoKV } from 'rxdb/plugins/storage-denokv';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+addRxPlugin(RxDBDevModePlugin);
+
 
 const myRxDatabase = await createRxDatabase({
-    name: 'exampledb',
-    storage: getRxStorageDenoKV({
-      /**
-       * Consistency level, either 'strong' or 'eventual'
-       * (Optional) default='strong'
-       */
-      consistencyLevel: 'strong',
-      /**
-       * Path which is used in the first argument of Deno.openKv(settings.openKvPath)
-       * (Optional) default=''
-       */
-      openKvPath: './deno-kv/deno-kv.db',
-      /**
-       * Some operations have to run in batches,
-       * you can test different batch sizes to improve performance.
-       * (Optional) default=100
-       */
-      batchSize: 100
-    })
+  name: 'exampledb',
+  storage: getRxStorageDenoKV({
+    /**
+     * Consistency level, either 'strong' or 'eventual'
+     * (Optional) default='strong'
+     */
+    consistencyLevel: 'strong',
+    /**
+     * Path which is used in the first argument of Deno.openKv(settings.openKvPath)
+     * (Optional) default=''
+     */
+    openKvPath: './deno-kv/deno-kv.db',
+    /**
+     * Some operations have to run in batches,
+     * you can test different batch sizes to improve performance.
+     * (Optional) default=100
+     */
+    batchSize: 100
+  })
 });
 
-const todoSchema = {
+
+const DDSchema = {
   version: 0,
   primaryKey: 'id',
   type: 'object',
   properties: {
-      id: {
-          type: 'string',
-          maxLength: 100 // <- the primary key must have set maxLength
-      },
-      name: {
-          type: 'string'
-      },
-      done: {
-          type: 'boolean'
-      },
-      timestamp: {
-          type: 'string',
-          format: 'date-time'
-      }
+    id: {
+      type: 'string',
+      maxLength: 100
+    },
+    CID: {
+      type: 'string',
+      maxLength: 100
+    },
+    previousCID: {
+      type: 'string',
+      maxLength: 100
+    },
+    content: {
+      type: 'object'
+    }
   },
-  required: ['id', 'name', 'done', 'timestamp']
+  required: ['id', 'CID', 'DD']
 }
 
+
+console.log(Object.keys(myRxDatabase.collections))
 await myRxDatabase.addCollections({
-  todos: {
-    schema: todoSchema
+  dd: {
+    schema: DDSchema
+  }
+});
+console.log(Object.keys(myRxDatabase.collections))
+
+
+const myDocument = await myRxDatabase.dd.upsert({
+  id: 'root',
+  CID: 'TODO',
+  previousCID: "TODO",
+  content: {
+    timestamp_ms: new Date()
   }
 });
 
 
-const myDocument = await myRxDatabase.todos.insert({
-  id: 'todo1',
-  name: 'Learn RxDB',
-  done: false,
-  timestamp: new Date().toISOString()
-});
-
-const foundDocuments = await myRxDatabase.todos.find({
+const foundDocuments = await myRxDatabase.dd.find({
   selector: {
-      done: {
-          $eq: false
-      }
+    id: {
+      $eq: 'root'
+    }
   }
 }).exec();
+
+
+console.log("\n")
+console.log("foundDocuments")
+console.log(Object.keys(foundDocuments[0]._data))
+
+
+console.log("List Collections")
+console.log(
+  Object.keys(myRxDatabase.collections)
+)
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+deno run -A --unstable-kv --unstable-broadcast-channel ./test.js
